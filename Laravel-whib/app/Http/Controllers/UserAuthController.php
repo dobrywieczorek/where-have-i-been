e7a3cd6cea448830f609ce37a58015aac6fc620d
MatWisie <Mateuszwisiecki@gmail.com>
2023-10-31 15:09:22 +0100

    Added RegisterUser method

--- Laravel-whib/app/Http/Controllers/UserAuthController.php
+++ Laravel-whib/app/Http/Controllers/UserAuthController.php
@@ -12,4 +12,18 @@ class UserAuthController extends Controller
 {
     function __construct(private readonly IUserAuthService $_userAuthService){}
 
+    public function RegisterUser(Request $request)
+    {
+        $user = $this->_userAuthService->AddUser($request);
+        if($user['success'] == false){
+            return response()->json(['errors'=>$user['errors']], 400);
+        }
+
+        $token = $this->_userAuthService->CreateUserToken($user['user']);
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
 }
