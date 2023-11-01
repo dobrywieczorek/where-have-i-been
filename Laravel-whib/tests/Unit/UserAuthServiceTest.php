eae6b7551d4f02572e99e970fae422d7ee9adbe1
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-01 13:39:30 +0100

    Added UserAuthServiceTest class

---
+++ Laravel-whib/tests/Unit/UserAuthServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Http\Interfaces\IUserAuthRepository;
+use App\Http\Services\UserAuthService;
+use Mockery;
+use Tests\TestCase;
+use Illuminate\Http\Request;
+
+class UserAuthServiceTest extends TestCase
+{
+
+
+
+}
+
