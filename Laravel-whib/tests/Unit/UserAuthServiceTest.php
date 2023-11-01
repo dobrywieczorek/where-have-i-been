e1df90170af3f04ee194c8e567a9f2d9710d7419
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-01 13:39:57 +0100

    Added test_AddUser_CorrectUser_ReturnsSuccessTrue test

--- Laravel-whib/tests/Unit/UserAuthServiceTest.php
+++ Laravel-whib/tests/Unit/UserAuthServiceTest.php
@@ -11,6 +11,27 @@ use Illuminate\Http\Request;
 
 class UserAuthServiceTest extends TestCase
 {
+    public function test_AddUser_CorrectUser_ReturnsSuccessTrue() : void
+    {
+        $userAuthRepository = Mockery::mock(IUserAuthRepository::class);
+
+        /** @var \Mockery\Mock|IUserAuthRepository $userAuthRepository */
+        $userAuthRepository->shouldReceive('AddUser')->andReturn(['name' => 'John', 'email' => 'john@example.com']);
+
+        $userAuthService = new UserAuthService($userAuthRepository);
+
+        $userData = [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'password' => 'Password1', 
+        ];
+
+        $request = Request::create('/dummy', 'POST', $userData);
+
+        $result = $userAuthService->AddUser($request);
+        $this->assertTrue($result['success']);
+    }
+
 
 
 
