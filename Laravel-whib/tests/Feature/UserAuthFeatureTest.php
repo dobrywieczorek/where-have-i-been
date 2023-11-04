3fc6536f655f5ea70d307035bd8784bed9530fab
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-04 23:41:05 +0100

    Added test_RegisterRoute_CorrectUser_200Response feature test

--- Laravel-whib/tests/Feature/UserAuthFeatureTest.php
+++ Laravel-whib/tests/Feature/UserAuthFeatureTest.php
@@ -9,4 +9,15 @@ class UserAuthFeatureTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_RegisterRoute_CorrectUser_200Response(): void
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john1@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $response->assertStatus(200);
+    }
+
 }
