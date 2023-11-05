09d8641cdc29d9ac35668fe23c36da9f3d63aff9
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2023-11-05 11:04:40 +0100

    Modified Google Controller

--- Laravel-whib/app/Http/Controllers/GoogleController.php
+++ Laravel-whib/app/Http/Controllers/GoogleController.php
@@ -9,5 +9,35 @@ use Illuminate\Support\Facades\Auth;
 use Exception;
 class GoogleController extends Controller
 {
+    public function googlepage()
+    {
+        return Socialite::driver('google')->redirect();
+    }
 
+    public function googleredirect()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if($finduser) {
+                Auth::login($finduser);
+                return redirect()->intended('dashboard');
+
+            } else {
+                $newUser = User::updateOrCreate([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password' => encrypt('')
+                ]);
+
+                Auth::login($newUser);
+                return redirect()->intended('dashboard');
+            }
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
 }
+
