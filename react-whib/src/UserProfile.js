b50f24201fa85abfab455a505cb26d1c55d374ed
Wiktor <109503651+wiktor-wojciechowski@users.noreply.github.com>
2023-11-05 22:38:12 +0100

    began profile page, removed unneeded css, changed app.js starting page for development

--- react-whib/src/UserProfile.js
+++ react-whib/src/UserProfile.js
@@ -1,5 +1,24 @@
+import './UserProfile.css'
 function UserProfile(){
-    return <h1>This is a profile page</h1>
+    return (
+        <div class="UserProfile">
+            <div class="profile-bg"></div>
+            <div class="nameHolder">
+                <h1 class="profile-name">Name</h1>
+                <button>Add Friend</button>
+            </div>
+            <span class="profile-date">Joined: Date</span>
+            <p class="profile-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fuga aliquam voluptate, nobis quaerat reprehenderit corrupti quo quisquam nostrum debitis nemo alias non. Magni nesciunt odit deserunt esse, quod inventore!</p>
+            <section>
+                <h2>Favorite Categories</h2>
+                <p>Category 1 Cat 2 etc...</p>
+            </section>
+            <section>
+                <h2>Pins</h2>
+                <p>Pinanme1 LocationX...</p>
+            </section>
+        </div>
+    );
 }
 
 export default UserProfile
