0f22ae010fb3d07ef7efeaf0d7896b0c1dd59899
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-07 11:55:36 +0100

    Created Friends migration and model

---
+++ Laravel-whib/app/Models/Friend.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Friend extends Model
+{
+    use HasFactory;
+
+    protected $table = 'friends';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function friendWithUser()
+    {
+        return $this->belongsTo(User::class, 'friend_with_user_id');
+    }
+}
