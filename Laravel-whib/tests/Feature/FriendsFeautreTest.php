e6399fed918dcf5a84b9ae82cfae5f8998a87837
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-08 13:31:45 +0100

    Added test_DeleteFriendRoute_IncorrectFriend_401Response feature test

--- Laravel-whib/tests/Feature/FriendsFeautreTest.php
+++ Laravel-whib/tests/Feature/FriendsFeautreTest.php
@@ -117,4 +117,36 @@ class FriendsFeautreTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_DeleteFriendRoute_IncorrectFriend_401Response(): void
+    {
+
+        $register1 = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john1@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $register2 = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john12@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $token = $register1['access_token'];
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/addfriend',[
+            'friend_id' => '2',
+        ]);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/deletefriend',[
+            'friend_id' => '3',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
 }
