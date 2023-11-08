d069d15f49ea8298be678a45e6cd27866af909f4
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-08 13:31:04 +0100

    Added test_AddFriendRoute_IncorrectFriend_401Response feature test

--- Laravel-whib/tests/Feature/FriendsFeautreTest.php
+++ Laravel-whib/tests/Feature/FriendsFeautreTest.php
@@ -34,4 +34,30 @@ class FriendsFeautreTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_AddFriendRoute_IncorrectFriend_401Response(): void
+    {
+
+        $register1 = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john1@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $register2 = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john12@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $token = $register1['access_token'];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/addfriend',[
+            'friend_id' => '0',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
 }
