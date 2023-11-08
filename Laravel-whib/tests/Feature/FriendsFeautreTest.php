5b39287de93499ece3ea8226eafcf64778671cb0
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-08 13:31:59 +0100

    Added test_DeleteFriendRoute_IdZero_401Response feature test

--- Laravel-whib/tests/Feature/FriendsFeautreTest.php
+++ Laravel-whib/tests/Feature/FriendsFeautreTest.php
@@ -149,4 +149,35 @@ class FriendsFeautreTest extends TestCase
         $response->assertStatus(401);
     }
 
+    public function test_DeleteFriendRoute_IdZero_401Response(): void
+    {
+
+        $register1 = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john1@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $register2 = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john12@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $token = $register1['access_token'];
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/addfriend',[
+            'friend_id' => '2',
+        ]);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/deletefriend',[
+            'friend_id' => '0',
+        ]);
+
+        $response->assertStatus(401);
+    }
 }
