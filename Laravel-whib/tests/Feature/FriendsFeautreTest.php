70bda5b92fc9db7da6a414e5f941c34128ccc7ff
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-08 13:30:21 +0100

    Added FriendsFeautreTest

---
+++ Laravel-whib/tests/Feature/FriendsFeautreTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FriendsFeautreTest extends TestCase
+{
+    use RefreshDatabase;
+
+}
