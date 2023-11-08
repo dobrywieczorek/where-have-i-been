c813d2b8ac257cd89609b7d98e6f65330bfc2117
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-08 13:28:12 +0100

    Added test_AddFriend_CorrectFriends_ReturnsSuccessTrue test

--- Laravel-whib/tests/Unit/FriendsServiceTest.php
+++ Laravel-whib/tests/Unit/FriendsServiceTest.php
@@ -10,4 +10,18 @@ use App\Http\Services\FriendsService;
 
 class FriendsServiceTest extends TestCase
 {
+    public function test_AddFriend_CorrectFriends_ReturnsSuccessTrue() : void
+    {
+        $friendsRepository = Mockery::mock(IFriendsRepository::class);
+
+        /** @var \Mockery\Mock|IUserAuthRepository $userAuthRepository */
+        $friendsRepository->shouldReceive('AlreadyFriends')->andReturn(false);
+        $friendsRepository->shouldReceive('AddFriend')->andReturn();
+
+        $friendsService = new FriendsService($friendsRepository);
+
+        $result = $friendsService->AddFriend(1, 2);
+        $this->assertTrue($result['success']);
+    }
+
 }
