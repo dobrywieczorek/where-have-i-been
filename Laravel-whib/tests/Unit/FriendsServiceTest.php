ee2cef1153e79030987507b47e95980742d8e1a3
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-08 13:27:57 +0100

    Added FriendsServiceTest

---
+++ Laravel-whib/tests/Unit/FriendsServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+use Mockery;
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use App\Http\Interfaces\IFriendsRepository;
+use App\Http\Interfaces\IFriendsService;
+use App\Http\Services\FriendsService;
+
+class FriendsServiceTest extends TestCase
+{
+}
