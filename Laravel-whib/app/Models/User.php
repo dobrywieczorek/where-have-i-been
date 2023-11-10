ad4b5d0ba303b1bdc142fbc7b263f14dc70f30e6
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-10 12:53:29 +0100

    Added friends and friendsWith methods to User model

--- Laravel-whib/app/Models/User.php
+++ Laravel-whib/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Friend;
 
 class User extends Authenticatable
 {
@@ -27,4 +28,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function friends()
+    {
+        return $this->hasMany(Friend::class, 'user_id');
+    }
+
+    public function friendsWith()
+    {
+        return $this->hasMany(Friend::class, 'friend_with_user_id');
+    }
 }
