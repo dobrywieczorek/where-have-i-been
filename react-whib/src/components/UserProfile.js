90b27146862fbf5428deed71663529db48e27e84
Wiktor <109503651+wiktor-wojciechowski@users.noreply.github.com>
2023-11-10 15:40:36 +0100

    changed class attributes in userprofile to className

--- react-whib/src/components/UserProfile.js
+++ react-whib/src/components/UserProfile.js
@@ -1,14 +1,15 @@
 import '../css/UserProfile.css'
 function UserProfile(){
     return (
-        <div class="UserProfile">
-            <div class="profile-bg"></div>
-            <div class="nameHolder">
-                <h1 class="profile-name">Name</h1>
+        <div className="UserProfile">
+            <div className="profile-bg"></div>
+            <div className="profile-img"><img></img></div>
+            <div className="nameHolder">
+                <h1 className="profile-name">Name</h1>
                 <button>Add Friend</button>
             </div>
-            <span class="profile-date">Joined: Date</span>
-            <p class="profile-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fuga aliquam voluptate, nobis quaerat reprehenderit corrupti quo quisquam nostrum debitis nemo alias non. Magni nesciunt odit deserunt esse, quod inventore!</p>
+            <span className="profile-date">Joined: Date</span>
+            <p className="profile-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fuga aliquam voluptate, nobis quaerat reprehenderit corrupti quo quisquam nostrum debitis nemo alias non. Magni nesciunt odit deserunt esse, quod inventore!</p>
             <section>
                 <h2>Favorite Categories</h2>
                 <p>Category 1 Cat 2 etc...</p>
