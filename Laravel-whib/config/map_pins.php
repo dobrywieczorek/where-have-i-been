4ff5c067bc62bb83dbc2693fa43cdd5dbd82675b
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2023-11-11 10:30:19 +0100

    Added config for maps

---
+++ Laravel-whib/config/map_pins.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'name' => '',
+        'description' => '',
+        'when' => '',
+        'userid' => '',
+        'istrip' => ''
+    ],
+    // Add more pins as needed
+];
