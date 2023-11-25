936f4d08bffc313791a965138f1b0847f224109b
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-25 14:31:44 +0100

    Added StatisticsRepostiory

---
+++ Laravel-whib/app/Http/Repositories/StatisticsRepostiory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Repositories;
+use App\Models\User;
+use App\Models\Friend;
+use App\Http\Interfaces\IStatisticsRepostiory;
+use Illuminate\Support\Facades\DB;
+
+class StatisticsRepostiory implements IStatisticsRepostiory
+{
+}
