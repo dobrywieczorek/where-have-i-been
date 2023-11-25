ca4eac580ba015110e11f75a1a393cfe1f751f97
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-26 00:09:50 +0100

    Added StatisticsFeatureTest

---
+++ Laravel-whib/tests/Feature/StatisticsFeatureTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StatisticsFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+}
