d51cbc59a175138e75c5fa23fdff94572b136032
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-26 00:10:03 +0100

    Added test_GetUserStats_CorrectData_200Response feature test

--- Laravel-whib/tests/Feature/StatisticsFeatureTest.php
+++ Laravel-whib/tests/Feature/StatisticsFeatureTest.php
@@ -8,5 +8,18 @@ class StatisticsFeatureTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_GetUserStats_CorrectData_200Response(): void
+    {
+        $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john1@example.com',
+            'password' => 'Password123',
+        ]);
+
+        $response = $this->withHeaders([])->get('/api/getUserStats?user_id=1',[]);
+
+        $response->assertStatus(200);
+    }
+
 
 }
