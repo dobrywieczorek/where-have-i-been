912efd4bc1528c77c1ca45821bf1cbfccb684b85
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-25 14:33:16 +0100

    Added GetStatistics to StatisticsService

--- Laravel-whib/app/Http/Services/StatisticsService.php
+++ Laravel-whib/app/Http/Services/StatisticsService.php
@@ -9,4 +9,24 @@ class StatisticsService implements IStatisticsService
 {
     function __construct(private readonly IStatisticsRepostiory $_statisticsRepostiory, private readonly IUserAuthService $_userAuthService){}
 
+    public function GetStatistics($request)
+    {
+        $user = $this->_userAuthService->GetCurrentUserWithToken($request);
+        if($user)
+        {
+            $numberOfPins = $this->_statisticsRepostiory->GetNumberOfUserPins($user);
+            $numberOfFriends = $this->_statisticsRepostiory->GetNumberOfUserFriends($user);
+            $numberOfObservers = $this->_statisticsRepostiory->GetNumberOfUsersObserving($user);
+            $mostUsedPinCategory = $this->_statisticsRepostiory->GetUserMostPopularPinCategory($user);
+            return 
+            [
+                'success' => true,
+                'numberOfPins' => $numberOfPins,
+                'numberOfFriends' => $numberOfFriends,
+                'numberOfObservers' => $numberOfObservers,
+                'mostUsedPinCategory' => $mostUsedPinCategory
+            ];
+        }
+        return ['success' => false, 'errors' => 'No user with such token'];
+    }
 }
