6f9ffd0db34bcc2eacfb73073fe5bbce010bc72c
MatWisie <Mateuszwisiecki@gmail.com>
2023-11-25 14:37:15 +0100

    Added GetUserStatistics to StatisticsController

--- Laravel-whib/app/Http/Controllers/StatisticsController.php
+++ Laravel-whib/app/Http/Controllers/StatisticsController.php
@@ -8,4 +8,18 @@ class StatisticsController extends Controller
 {
     function __construct(private readonly IStatisticsService $_statisticsService){}
 
+    public function GetUserStatistics(Request $request)
+    {
+        $statistics = $this->_statisticsService->GetStatistics($request);
+        if($statistics['success']==true)
+        {
+            return response()->json([
+                'numberOfPins' => $statistics['numberOfPins'],
+                'numberOfFriends' => $statistics['numberOfFriends'],
+                'numberOfObservers' => $statistics['numberOfObservers'],
+                'mostUsedPinCategory' => $statistics['mostUsedPinCategory']
+            ]);
+        }
+        return response()->json(['errors'=>"Invalid request"], 400);
+    }
 }
