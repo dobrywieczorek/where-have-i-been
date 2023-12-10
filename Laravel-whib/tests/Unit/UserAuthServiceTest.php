0b47b56f13efa5399781b7b25e8fa20db0a1593d
MatWisie <Mateuszwisiecki@gmail.com>
2023-12-10 14:40:03 +0100

    Added test_AddUser_InvalidName_ReturnsSuccessFalse test

--- Laravel-whib/tests/Unit/UserAuthServiceTest.php
+++ Laravel-whib/tests/Unit/UserAuthServiceTest.php
@@ -13,7 +13,7 @@ class UserAuthServiceTest extends TestCase
 {
 
     private array $rules = [
-        'name' => 'required|string|max:255',
+        'name' => 'required|string|max:255|regex:/^[A-Za-z0-9\s]+$/',
         'email' => 'required|string|email|max:255',
         'password' => 'required|string|min:8|regex:/^(?=.*[A-Z])(?=.*\d).+$/',
     ];
@@ -112,7 +112,28 @@ class UserAuthServiceTest extends TestCase
         $userData = [
             'name' => 'John',
             'email' => '',
-            'password' => 'password1', 
+            'password' => 'Password1', 
+        ];
+
+        $request = Request::create('/dummy', 'POST', $userData);
+
+        $result = $userAuthService->AddUser($request, $this->rules);
+        $this->assertFalse($result['success']);
+    }
+
+    public function test_AddUser_InvalidName_ReturnsSuccessFalse() : void
+    {
+        $userAuthRepository = Mockery::mock(IUserAuthRepository::class);
+
+        /** @var \Mockery\Mock|IUserAuthRepository $userAuthRepository */
+        $userAuthRepository->shouldReceive('AddUser')->andReturn(['name' => 'John', 'email' => 'john@example.com']);
+
+        $userAuthService = new UserAuthService($userAuthRepository);
+
+        $userData = [
+            'name' => '#John john',
+            'email' => 'blabla@bla.com',
+            'password' => 'Password1', 
         ];
 
         $request = Request::create('/dummy', 'POST', $userData);
