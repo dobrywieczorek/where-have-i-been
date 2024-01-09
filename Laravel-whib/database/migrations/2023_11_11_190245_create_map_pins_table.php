83082b282298c7230e468c5ad2f472cee2062dd5
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-01-09 15:57:45 +0100

    1

--- Laravel-whib/database/migrations/2023_11_11_190245_create_map_pins_table.php
+++ Laravel-whib/database/migrations/2023_11_11_190245_create_map_pins_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->decimal('longitude', 11, 8);
             $table->foreignId('user_id')->constrained('users');
             $table->string('category');
+            $table->boolean('isTrip');
             $table->timestamps();
         });
     }
